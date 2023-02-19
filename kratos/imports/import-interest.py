bb1ea3ced0358ab07b242ed468855fa5b43a6cb4
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-02-19 11:35:25 +0100

    refactor(kratos-interest): minor script improvement

--- kratos/imports/import-interest.py
+++ kratos/imports/import-interest.py
@@ -7,7 +7,6 @@ try:
     )
     if not mysql_connection.is_connected():
         raise Exception("Could not connect to mysql database")
-
     postgres_connection = psycopg2.connect(
         database="kratos",
         host="localhost",
@@ -19,22 +18,18 @@ try:
     # We don't need to check what is in columns `field` and `value`
     # because in legacy all of them are 'interests' and 'teacher' respectively
     mysql_cursor.execute("select user_id from user_field")
-
     user_field_result = mysql_cursor.fetchall()
     user_ids = tuple(map(lambda x: str(x), map(lambda x: x[0], user_field_result)))
 
     postgres_cursor = postgres_connection.cursor()
-
-    # Introduce interest field
     postgres_cursor.execute(
-        'update identities set traits = traits || \'{"interest": ""}\''
-    )
-
-    postgres_cursor.execute(
-        "update identities set traits = traits || '{\"interest\": \"teacher\"}' where metadata_public ->> 'legacy_id' in %s",
+        "UPDATE identities SET traits = traits || '{\"interest\": \"\"}' WHERE metadata_public ->> 'legacy_id' NOT IN %s",
         [user_ids],
     )
-
+    postgres_cursor.execute(
+        "UPDATE identities SET traits = traits || '{\"interest\": \"teacher\"}' WHERE metadata_public ->> 'legacy_id' IN %s",
+        [user_ids],
+    )
     postgres_connection.commit()
 except Exception as error:
     raise error
