ac092316a24eac2620b3d5efac4178a82d35604e
Stephan Kulla <git.mail@kulla.me>
2023-07-16 23:46:50 +0200

    Add script `docker-compose.sh`
    So that you can run `scripts/docker-compose.sh ps` or
    `scripts/docker-compose.sh logs mysql`.

---
+++ scripts/docker-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export $(grep -v '^#' .env | xargs)
+
+for filename in docker/*.yml; do
+  all_docker_compose_files="$all_docker_compose_files -f $filename"
+done
+
+docker-compose $all_docker_compose_files "$@"
